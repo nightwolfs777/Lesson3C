82b8f43dd2c08175d279b4318d73af92b467c689
IldarPC <Ildarka.201995@gmail.com>
2023-03-16 16:56:49 +0500

    Work23

--- Work23/Program.cs
+++ Work23/Program.cs
@@ -1,7 +1,18 @@
-﻿// Задача 23
+﻿#pragma warning disable
+// Задача 23
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+
+int N;
+Console.Write($"Введите число N:");
+int.TryParse(Console.ReadLine()!, out N);
+
+for (int i = 1; i <= N; i++)
+{
+    double x = Math.Pow(i, 3);
+    Console.Write($"{x}, ");
+}
